1316632e2beea83cdbccd96abb3aa83698011971
nick Frattaroli <nafratt@gmail.com>
2020-02-25 16:37:10 -0500

    making progress

--- assets/Scripts/Javascript.js
+++ assets/Scripts/Javascript.js
@@ -5,20 +5,22 @@ const e = React.createElement;
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = { clicked: false };
   }
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
+    if (this.state.clicked) {
+      return(
+        document.write("")
+      ) ;    }
 
     return e(
       'button',
-      { onClick: () => this.setState({ liked: true }) },
+      { onClick: () => this.setState({ clicked: true }) },
       'Like'
     );
   }
 }
 const domContainer = document.querySelector('#like_button_container');
 ReactDOM.render(e(LikeButton), domContainer);
+
